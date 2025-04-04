7bc536508c98e5fd557d2a43ba8db25746ea5b05
Philip Holm Møller <phmo61881@edu.ucl.dk>
2025-04-04 14:14:13 +0200

    trying to optimize

--- BlazorFoodWasteProject/Client/Program.cs
+++ BlazorFoodWasteProject/Client/Program.cs
@@ -1,11 +1,19 @@
 using BlazorFoodWasteProject.Client;
+using BlazorFoodWasteProject.Client.Services.Interfaces;
+using BlazorFoodWasteProject.Client.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// DI for FoodWasteService
+builder.Services.AddScoped<IFoodWasteService, FoodWasteService>();
+
 
 await builder.Build().RunAsync();
